18d59c658ebeff6f3ecacd8a215d1a0e5db6b787
Athirakrishnan98 <athirakrishnan98rg@gmail.com>
2025-01-19 23:56:47 +0530

    setup.ts change

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Staring up.................");
+  console.log("Staring up...");
 
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
